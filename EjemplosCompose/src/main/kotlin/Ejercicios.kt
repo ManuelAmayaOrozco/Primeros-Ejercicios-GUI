81ef6ede6e14dcb686b42f88ea4bac210d21a8fe
Manuel <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2024-04-22 19:23:09 +0200

    Inicio

---
+++ EjemplosCompose/src/main/kotlin/Ejercicios.kt
@@ -0,0 +1,41 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+@Composable
+fun Ejercicio1() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center)
+    {
+        Box(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .background(Color.Cyan)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+fun main() = application {
+    val windowState = rememberWindowState(size = DpSize(1200.dp, 800.dp))
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Ejercicios",
+        state = windowState
+    ) {
+        Ejercicio1()
+    }
+}
