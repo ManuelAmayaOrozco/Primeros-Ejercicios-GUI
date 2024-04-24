e0efcb074f42645bcbc155764dc764062fd2be59
Manuel <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2024-04-24 12:02:51 +0200

    Finalizado (A medias)
    He intentado hacer los ejercicios aunque la mayoría los tengo mal ya que no logro captar bien los conceptos y tengo varias dudas de como funciona cada cosa, especialmente en cuanto a partes del modifier como weight y fillMaxSize/fillMaxWidth.

--- EjemplosCompose/src/main/kotlin/Ejercicios.kt
+++ EjemplosCompose/src/main/kotlin/Ejercicios.kt
@@ -10,7 +10,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -159,9 +161,189 @@ fun Ejercicio4() {
                     )
                 )
             }
+            Row(
+                modifier = Modifier
+                    .height(150.dp)
+                    .background(Color.White)
+                    .wrapContentSize(Alignment.TopCenter)
+                    .border(BorderStroke(5.dp,Color.Red))
+            ) {
+                Text(
+                    text = "Ejemplo 3",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .height(150.dp)
+                    .background(Color.White)
+                    .wrapContentSize(Alignment.TopCenter)
+                    .border(BorderStroke(5.dp,Color.Blue))
+            ) {
+                Text(
+                    text = "Ejemplo 4",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
         }
     }
 }
+
+@Composable
+fun Ejercicio5() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center)
+    {
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+                .background(Color.Cyan)
+                .align(Alignment.TopCenter)
+                .wrapContentSize(Alignment.Center)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "BOX 1",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(150.dp))
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+                .background(Color.Gray)
+                .align(Alignment.TopCenter)
+                .wrapContentSize(Alignment.Center)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "BOX 2",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(150.dp))
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+                .background(Color.Green)
+                .align(Alignment.TopCenter)
+                .wrapContentSize(Alignment.Center)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "BOX 3",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(150.dp))
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+                .background(Color.Magenta)
+                .align(Alignment.TopCenter)
+                .wrapContentSize(Alignment.Center)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "BOX 3",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun Ejercicio6() {
+    Column(modifier = Modifier.fillMaxSize())
+    {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                .background(Color.Cyan)
+                .weight(1F)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Ejercicio 2",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    color = Color.Blue,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier
+                    .border(BorderStroke(10.dp,Color.Black))
+                    .background(Color.Yellow)
+                    .padding(10.dp)
+            )
+        }
+        Row(
+
+        ) {
+            Box(
+                contentAlignment = Alignment.BottomCenter,
+                modifier = Modifier
+                    .background(Color.Blue)
+                    .weight(1F)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    text = "PMDM",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .background(Color.Green)
+                    .weight(1F)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    text = "DAM 2",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        color = Color.DarkGray,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+        }
+        Box(
+            contentAlignment = Alignment.BottomEnd,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .weight(1F)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "...Combinando Column y Box",
+                style = TextStyle(
+                    fontSize = 22.sp,
+                    color = Color.Yellow,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+}
+
+//Ejercicio 7 ya no tengo ni idea
 
 fun main() = application {
     val windowState = rememberWindowState(size = DpSize(1200.dp, 800.dp))
@@ -171,6 +353,6 @@ fun main() = application {
         title = "Ejercicios",
         state = windowState
     ) {
-        Ejercicio4()
+        Ejercicio6()
     }
 }
